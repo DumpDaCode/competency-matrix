d5d5d75fe6fb0a0267b3bea4f3cb4fe811914db4
Rajivranjan <rajivranjans@mkcl.org>
2021-09-02 13:53:14 +0530

    nothing special

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <AdminMainPage></AdminMainPage>
+  </div>
 </template>
 
+<script>
+import AdminMainPage from "./views/AdminMainPage.vue";
+export default {
+  name: "App",
+  components: {
+    AdminMainPage
+  }
+}
+</script>
 <style>
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-light container-content">
+        <div class="rounded-3 bg-white container my-5 container-content">
+            ASdad
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container-content{
+    width: 100vw;
+}
+</style>
